1e3ac443238f4afa97c870565887cbf74fc68922
Ankit Raj <imankitrj@gmail.com>
2025-05-13 15:18:56 +0530

    grep refactor

--- grep/src/main.rs
+++ grep/src/main.rs
@@ -4,7 +4,6 @@ use grep;
 use std::process;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
 
     let config = grep::Config::new(&args).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
